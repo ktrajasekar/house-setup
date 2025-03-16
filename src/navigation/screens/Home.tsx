7f98c03ffce2813903b74b6d4155732c65024fa1
Rajasekar Thangavel <kt.rajasekar@gmail.com>
2025-03-16 19:31:51 +0530

    New Articles List

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,31 +1,71 @@
 // import { Button, Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import TextInputComponent from '../../components/TextInputComponent';
-import React from 'react';
+import { StyleSheet, View, FlatList, Text, ScrollView } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { useEffect } from 'react';
+
 const hello = () => {
   console.log('Hello');
 }
 export function Home() {
+  const [articles, setArticles] = React.useState<{ title: string, description: string }[]>([]);
   const [text, onChangeText] = React.useState('Useless Text');
+  const getNewsList = async () => {
+    try {
+      const response = await fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=9c96b28ea56a4c8e9bc5e83908969662');
+      const json = await response.json();
+      setArticles(json.articles);
+    } catch (error) {
+      console.error(error); // Error handling
+    }
+  }
+  useEffect(() => {
+    getNewsList();
+  }, []);
   return (
-    <View style={styles.container}>
-      {/* <Text>Home Screen</Text>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* <Text>Home Screen</Text>
       <Text>Open up 'src/App.tsx' to start working on your app!</Text>
       <Button onPress={() => hello()}>Press me</Button>
       <Button screen="Profile" params={{ user: 'jane' }}>
         Go to Profile
       </Button>
       <Button screen="Settings">Go to Settings</Button> */}
-      <TextInputComponent onChangeText={(text) => console.log(text)} placeholder='Enter sq ft' />
-      <View style={{ height: 20 }} />
-    </View>
+        {/* <TextInputComponent onChangeText={(text) => console.log(text)} placeholder='Enter sq ft' /> */}
+        <ScrollView style={styles.NewsContainer}>
+          <FlatList
+            data={articles}
+            renderItem={({ item }) => <View style={styles.Article}>
+              <Text style={styles.ArticleTitle}>{item.title}</Text>
+              <Text style={styles.ArticleDesc}>{item.description}</Text>
+            </View>}
+            keyExtractor={(item, index) => item.title?.toString() || index.toString()}
+          />
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    height: '100%',
+  },
+  NewsContainer: {
+    padding: 20,
+  },
+  Article: {
+    marginBottom: 20,
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  ArticleDesc: {
+    fontSize: 12,
+  },
+  ArticleTitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
   Input: {
     height: 40,
